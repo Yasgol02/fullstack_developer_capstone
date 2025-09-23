db6ab9904bc0d8ce1c90b3606495cea0eefcb7aa
Yasgol02 <145162103+Yasgol02@users.noreply.github.com>
2025-09-22 22:00:15 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -9,6 +9,7 @@ def initiate():
         {"name":"Toyota", "description":"Great cars. Japanese technology"},
     ]
 
+    
     car_make_instances = []
     for data in car_make_data:
             car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
@@ -34,5 +35,6 @@ def initiate():
         # Add more CarModel instances as needed
     ]
 
+    
     for data in car_model_data:
             CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
