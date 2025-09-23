ba86e610ff91d998055e3e8dc7749d6e0757f01f
Yasgol02 <145162103+Yasgol02@users.noreply.github.com>
2025-09-22 22:12:21 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,22 +2,40 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
 from django.views.generic import TemplateView
+from . import views
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
+
 urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name='register'), 
+    # path for registration
+    path("register", views.registration, name="register"),
     # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_request, name="logout"),
+    path("get_cars", views.get_cars, name="getcars"),
     # path for dealer reviews view
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>/', view=views.get_dealer_reviews, name='dealer_reviews'),
-    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='post_review'),    # path for add a review view
-    path(route='add_review/', view=views.add_review, name='add_review'),
+    path("get_dealers/", views.get_dealerships, name="get_dealers"),
+    path(
+        "get_dealers/<str:state>",
+        views.get_dealerships,
+        name="get_dealers_by_state",
+    ),
+    path(
+        "dealer/<int:dealer_id>/",
+        views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        "reviews/dealer/<int:dealer_id>/",
+        views.get_dealer_reviews,
+        name="dealer_reviews",
+    ),
+    path(
+        "postreview/<int:dealer_id>/",
+        TemplateView.as_view(template_name="index.html"),
+        name="post_review",
+    ),
+    # path for add a review view
+    path("add_review/", views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
